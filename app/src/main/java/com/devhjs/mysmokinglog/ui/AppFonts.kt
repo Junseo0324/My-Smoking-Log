4ee0643333a4a29918d10943cd3feee6d4b144fc
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-12-17 18:00:05 +0900

    chore: Font 설정

---
+++ app/src/main/java/com/devhjs/mysmokinglog/ui/AppFonts.kt
@@ -0,0 +1,16 @@
+package com.devhjs.mysmokinglog.ui
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.devhjs.mysmokinglog.R
+
+object AppFonts {
+
+    object AppFonts {
+        val Pretendard = FontFamily(
+            Font(R.font.pretendard_regular, FontWeight.Normal),
+            Font(R.font.pretendard_semibold, weight = FontWeight.SemiBold)
+        )
+    }
+}
