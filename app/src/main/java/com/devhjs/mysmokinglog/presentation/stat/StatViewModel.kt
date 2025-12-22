07d18b2ecae1736572f269e668986382a8e6d3b5
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-12-22 23:45:09 +0900

    feat: StatScreen viewModel 작성

---
+++ app/src/main/java/com/devhjs/mysmokinglog/presentation/stat/StatViewModel.kt
@@ -0,0 +1,37 @@
+package com.devhjs.mysmokinglog.presentation.stat
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.devhjs.mysmokinglog.domain.usecase.GetStatUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class StatViewModel @Inject constructor(
+    private val getStatUseCase: GetStatUseCase
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(StatState())
+    val state: StateFlow<StatState> = _state.asStateFlow()
+
+    init {
+        loadStats()
+    }
+
+    private fun loadStats() {
+        viewModelScope.launch {
+            getStatUseCase()
+                .collect { stats ->
+                    _state.value = stats
+                }
+        }
+    }
+    
+    fun refresh() {
+        loadStats()
+    }
+}
