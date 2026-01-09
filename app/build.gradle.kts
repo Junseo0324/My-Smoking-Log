c8eb1ef7fbbeeade7d5738086f38448f441d0c59
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-01-09 10:53:54 +0900

    chore: 앱 업데이트를 위한 versionCode 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.devhjs.mysmokinglog"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "com.devhjs.mysmokinglog.CustomTestRunner"
     }
